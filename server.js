b60ed867f78514abac53723fa35503d503497b7f
Luis Quintana Avila <quintana2427@gmail.com>
2026-07-09 13:37:26 +0000

    Clase 52 dashboard seguimiento

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ require("dotenv").config();
 const express = require("express");
 const path = require("path");
 const peticionesRoutes = require("./src/routes/peticiones.routes");
-
 const authRoutes = require("./src/routes/auth.routes");
+const dashboardRoutes = require("./src/routes/dashboard.routes");
+
 
 const app = express();
 
@@ -13,12 +14,14 @@ const APP_NAME =
   process.env.APP_NAME || "Plataforma de Orientación y Peticiones Ciudadanas";
 const APP_ENV = process.env.APP_ENV || "development";
 
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/api/peticiones", peticionesRoutes);
 app.use("/api/auth", authRoutes);
+app.use("/api/dashboard", dashboardRoutes);
 
 app.get("/", (req, res) => {
   res.send(`
